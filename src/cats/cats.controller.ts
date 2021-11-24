082844bf0d410628f1ba3aff234c0e489c3c1d17
Maksim <the.butterfly.incorporated@gmail.com>
2021-11-24 19:36:49 +0300

    Add info about ip and host

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Req, Ip, HostParam } from "@nestjs/common";
+import { Request } from "express";
 
 @Controller("cats")
 export class CatsController {
   @Get()
-  findAll(): string {
-    return "This action return cats";
+  findAll(
+    @Req() request: Request,
+    @Ip() ip: string,
+    @HostParam() host: string
+  ): string {
+    console.log(`${ip} ${host}`);
+    return `${ip} ${host} This action return cats`;
   }
 }
