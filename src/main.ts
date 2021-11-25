0dfb188b2901ba6cde9d7cf3f8e21b11663c1b7d
Maksim <the.butterfly.incorporated@gmail.com>
2021-11-25 19:10:13 +0300

    Create global filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { AllExceptionFilter } from "./common/filters/all-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //const { httpAdapter } = app.get(HttpAdapterHost);
+  //app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
   await app.listen(3000);
 }
 bootstrap();
