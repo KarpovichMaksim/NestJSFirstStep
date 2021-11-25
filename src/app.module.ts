98067985e128c0e15aefb4689e27cb1a906db163
Maksim <the.butterfly.incorporated@gmail.com>
2021-11-25 12:43:44 +0300

    Add some restriction for middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CatsModule } from "./cats/cats.module";
@@ -11,6 +16,10 @@ import { LoggerMiddleware } from "./common/middleware/logger.middleware";
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes("cats");
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: "cats", method: RequestMethod.POST })
+      .forRoutes({ path: "cats", method: RequestMethod.GET });
+    //.forRoutes(CatsController);
   }
 }
