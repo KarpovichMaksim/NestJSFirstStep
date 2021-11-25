6446335453e2693a0f6edd2b13590c8a4076be79
Maksim <the.butterfly.incorporated@gmail.com>
2021-11-25 21:50:59 +0300

    Add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { HttpAdapterHost, NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { AllExceptionFilter } from "./common/filters/all-exception.filter";
+import { ValidationPipe } from "./common/pipes/validation.pipe";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //const { httpAdapter } = app.get(HttpAdapterHost);
   //app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
