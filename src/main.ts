9ec881ca86de634afa01a8752fe4fb4ac333c9d5
Maksim <the.butterfly.incorporated@gmail.com>
2021-11-26 23:56:21 +0300

    Use global interceptors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { HttpAdapterHost, NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { AllExceptionFilter } from "./common/filters/all-exception.filter";
 import { AuthGuard } from "./common/guards/auth.guard";
+import { LoggingInterceptor } from "./common/interceptors/logging.interceptor";
 import { ValidationPipe } from "./common/pipes/validation.pipe";
 
 async function bootstrap() {
@@ -10,6 +11,7 @@ async function bootstrap() {
   //app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
   //app.useGlobalPipes(new ValidationPipe());
   //app.useGlobalGuards(new AuthGuard());
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(3000);
 }
 bootstrap();
