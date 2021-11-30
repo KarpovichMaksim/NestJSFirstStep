159836b94ba221d396bee3ddd2a8f0d6c9003175
Maksim <the.butterfly.incorporated@gmail.com>
2021-11-30 21:32:56 +0300

    Create config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CatsModule } from "./cats/cats.module";
 import { LoggerMiddleware } from "./common/middleware/logger.middleware";
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
