e147012d36aff02e709e467a69c28a91754b00df
Maksim <the.butterfly.incorporated@gmail.com>
2021-11-30 23:07:20 +0300

    Add lazy louding for lazy module

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import { HttpAdapterHost, LazyModuleLoader, NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { AllExceptionFilter } from "./common/filters/all-exception.filter";
 import { AuthGuard } from "./common/guards/auth.guard";
@@ -7,6 +7,10 @@ import { ValidationPipe } from "./common/pipes/validation.pipe";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //lazy module louder
+  const lazyModuleLoader = app.get(LazyModuleLoader);
+  const { LazyModule } = await import("./lazy/lazy.module");
+  const moduleRef = await lazyModuleLoader.load(() => LazyModule);
   //const { httpAdapter } = app.get(HttpAdapterHost);
   //app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
   //app.useGlobalPipes(new ValidationPipe());
