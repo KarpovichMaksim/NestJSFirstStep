a16ab7cf6ef1bde82985b029a89da190be3fb12c
Maksim <the.butterfly.incorporated@gmail.com>
2021-12-01 23:14:33 +0300

    Add livesycle hook module init

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit } from "@nestjs/common";
 import { Cat } from "./interfaces/cat.interface";
 
 @Injectable()
-export class CatsService {
+export class CatsService implements OnModuleInit {
   private readonly cats: Cat[] = [];
+
+  onModuleInit() {
+    console.log("The module has been initialization");
+  }
 
   create(cat: Cat) {
     this.cats.push(cat);
